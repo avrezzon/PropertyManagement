f27bd48a70d801b128871010859c7b67ddb27792
Anthony Rezzonico <31713372+avrezzon@users.noreply.github.com>
2025-11-20 01:09:47 -0700

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/PropertyManagement/',
     plugins: [react()],
     test: {
         globals: true,
